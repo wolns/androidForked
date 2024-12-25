d0b394bc07a16015c27c05c53ca8d705b7d67050
Oleg <Sal1r57@yandex.ru>
2024-12-25 22:26:24 +0300

    filling me screen

--- app/src/main/java/com/wolns/android/presentation/screens/me/MeScreen.kt
+++ app/src/main/java/com/wolns/android/presentation/screens/me/MeScreen.kt
@@ -1,15 +1,33 @@
 package com.wolns.android.presentation.screens.me
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wolns.android.R
+import com.wolns.android.presentation.ui.theme.WolnsTheme
 
 @Composable
 fun MeScreen() {
@@ -23,10 +41,56 @@ private fun MeScreenContent() {
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.surface)
     ) {
-        Box(
-            contentAlignment = Alignment.Center
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
-            Text("Me Screen")
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(96.dp)
+                        .clip(CircleShape)
+                )
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column {
+                    Text(
+                        text = "Your name",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                    Text(
+                        text = "@yourLogin",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            FilterChip(
+                selected = true,
+                onClick = { /*TODO*/ },
+                label = {
+                    Text(
+                        text = "Connect Yandex Music"
+                    )
+                }
+            )
+            FilterChip(
+                selected = true,
+                enabled = false,
+                onClick = { /*TODO*/ },
+                label = {
+                    Text(
+                        text = "Connect Yandex Music"
+                    )
+                }
+            )
         }
     }
 }
@@ -34,5 +98,10 @@ private fun MeScreenContent() {
 @Preview
 @Composable
 private fun MeScreenPreview() {
-
+    WolnsTheme(
+        darkTheme = true,
+        dynamicColor = false
+    ) {
+        MeScreenContent()
+    }
 }
